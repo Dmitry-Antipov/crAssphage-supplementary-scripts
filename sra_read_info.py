64b4fcc5c31acda9f789b3da334339cfad9254d1
Mike Rayko <mrayko@chihua.cab.spbu.ru>
2020-07-06 22:56:08 +0300

    script for sra metadata extraction

--- sra_read_info.py
+++ sra_read_info.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 from Bio import Entrez
 import sys
 
@@ -7,31 +6,55 @@ sra_list=[]
 for line in open(sys.argv[1], "r"):
      sra_list.append(line.strip().split()[0])
 
-Entrez.email = "mike.rayko@gmail.com"
 
 
-retmax = 500
-for i in range (0, len(sra_list),retmax):
-    id_list = ",".join(sra_list[i: min(i+retmax, len(sra_list))])
+def get_metadata(sra_list):
+    Entrez.email = "mike.rayko@gmail.com"
+    retmax = 200
 
-    try:
-        handle = Entrez.efetch(db="sra", id = id_list, rettype="runinfo", retmode="text")
-    except:
-        print (f'Failed in {str(i)} - {min(i+retmax, len(sra_list))}')
-        continue
-
-    meta = handle.readlines()
-    for i in range (1, len(meta)):
-        if len(meta[0].split(",")) != len(meta[i].split(",")) or meta[0].split(",")[0] == meta[i].split(",")[0] :
+    out_dict = {}
+    for i in range (0, len(sra_list),retmax):
+        id_list = ",".join(sra_list[i: min(i+retmax, len(sra_list))])
+    
+        try:
+            handle = Entrez.efetch(db="sra", id = id_list, rettype="runinfo", retmode="text")
+        except:
+            print (f'Failed in {str(i)} - {min(i+retmax, len(sra_list))}')
             continue
-        else:
-            meta_dict = dict(zip(meta[0].split(","), meta[i].split(",")))
-            if meta_dict["Platform"] != "ILLUMINA":
-                print(meta_dict["Run"] + "\t "+ "Non_illumina")
-            else:
-                if meta_dict["LibraryLayout"] == "PAIRED":
-                    print(meta_dict["Run"] + "\t "+ str(int(meta_dict["avgLength"])/2)) 
+        meta = []
+        for line in handle.readlines():
+            if len(line.strip().split(","))>1:
+                if '"' in line:
+                    heid = line.split('"')
+                    meta.append((heid[0] + heid[1].replace(","," ") + heid[2]).strip().split(","))
                 else:
-                    print(meta_dict["Run"] + "\t "+ str(int(meta_dict["avgLength"])))
-    handle.close()
+                    meta.append(line.strip().split(","))
 
+        for k in range (1, len(meta)):
+            if len(meta[0]) != len(meta[k]) or meta[0][0] == meta[k][0]:
+                continue
+            else:
+                meta_dict = dict(zip(meta[0], meta[k]))
+                out_dict[meta[k][0]]=meta_dict
+        handle.close()
+
+    for i in sra_list:
+        if i not in out_dict:
+            meta_dict = {x: "NA" for x in meta_dict}
+            out_dict[i]=meta_dict
+
+
+    return (out_dict)
+    
+
+meta = get_metadata(sra_list)
+
+for i in sra_list:
+    if meta[i]["Platform"] != "ILLUMINA":
+        print(i + "\t "+ "Non_illumina")
+    else:
+        if meta[i]["LibraryLayout"] == "PAIRED":
+            print(f'{i} \t {str(int(meta[i]["avgLength"])/2)} \t {meta[i]["LibraryStrategy"]}') 
+        else:
+            print(f'{i} \t {str(int(meta[i]["avgLength"]))} \t {meta[i]["LibraryStrategy"]}')
+            
